f78ced70a5e99b6ecc4eb46c73d3724084fbba07
JrGustavo <gus@soypetal.com>
2023-07-08 12:53:24 -0500

    Update JavaScript

---
+++ src/bases/10-async.js
@@ -0,0 +1,29 @@
+// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
+// getImagenPromesa().then( console.log );
+
+const getImagen = async() => {
+
+    try {
+
+        const apiKey = '';
+        const resp   = await fetch(``);
+        const { data } = await resp.json();
+
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+
+    } catch (error) {
+        // manejo del error
+        console.error(error)
+    }
+
+
+
+}
+
+getImagen();
+
+
